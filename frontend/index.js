ddef3e481c4cc7957ef2eb3b6dbb9c05e251f3e3
WaifuForever <itachiut1r4@gmail.com>
2020-08-03 22:24:01 -0300

    Update Ready

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,59 @@
+function createRow(){
+    
+    var value = document.getElementsByClassName('task-title')[0]
+    .getElementsByTagName('input')[0].value;
+   
+    const div = document.createElement('div');
+    var div_id = Math.random().toString()
+
+    div.className = 'row';
+
+    
+    div.id = div_id.toString();
+    //var data = {text: value, id: div_id};
+
+    var data = [value.toString(), div_id]
+
+    data = JSON.stringify(data)
+  
+    div.innerHTML = `
+        
+        <img class="check" src="./assets/circle-icon.png" alt="" onclick="check(${div_id.toString()})">
+        <img class="checked" src="./assets/right-icon.png" alt="" onclick="check(${div_id.toString()})">
+
+        <div class="task">${value}</div>
+        
+        <div class="button edit">
+            <img src="./assets/edit-icon.png" alt="" onclick="DisplayPopup('edit-popup', ${div_id.toString()})">
+        </div>
+        <div class="button trash">
+            <img src="./assets/trash-icon.png" alt="" onclick="DeleteRow(${div_id.toString()})">
+        </div>
+            
+
+        
+    `;
+  
+    document.getElementsByClassName('rows')[0].appendChild(div);
+}
+
+
+function UpdateRow(id, text){
+    document.getElementById(id).getElementsByClassName('task')[0].textContent = text;
+    let print = document.getElementById(id).getElementsByClassName('task')[0].textContent;
+    console.log(print, ' ', id )
+    
+
+}
+
+function DeleteRow(id){
+    var row = document.getElementById(id)
+
+    document.getElementsByClassName('rows')[0].removeChild(row);
+      
+
+}
+
 function check(id){
     
     var row = document.getElementById(id);
@@ -20,53 +76,68 @@ function check(id){
    
 }
 
-function createRow(){
+
+
+function CancelPopup(str){
+    document.getElementsByClassName(str)[0].style.display = "none";
     
-    var value = document.getElementsByClassName('task-title')[0]
-    .getElementsByTagName('input')[0].value;
-   
-    const div = document.createElement('div');
-    var div_id = Math.random().toString()
-
-    div.className = 'row';
-
-    
-    div.id = div_id.toString();
-  
-    div.innerHTML = `
-        
-        <img class="check" src="./assets/circle-icon.png" alt="" onclick="check(${div_id.toString()})">
-        <img class="checked" src="./assets/right-icon.png" alt="" onclick="check(${div_id.toString()})">
-
-        <div class="task">${value}</div>
-        
-        <div class="button edit">
-            <img src="./assets/edit-icon.png" alt="">
-        </div>
-        <div class="button trash">
-            <img src="./assets/trash-icon.png" alt="" onclick="RemoveRow(${div_id.toString()})">
-        </div>
-            
-
-        
-    `;
-  
-    document.getElementsByClassName('rows')[0].appendChild(div);
 }
 
-function RemoveRow(id){
-    var row = document.getElementById(id)
+function EditPopup(id){    
+    let popup = document.getElementsByClassName('edit-popup')[0]
 
-    document.getElementsByClassName('rows')[0].removeChild(row);
-      
+    let input = popup.getElementsByTagName('input')[0];
 
-}
-
-function CancelPopup(){
+    let row = document.getElementById(id);    
+    let row_color = row.style.backgroundColor;
+    
+    
     document.getElementsByClassName('form-popup')[0].style.display = "none";
+    popup.style.display = "flex";
+
+    row.style.backgroundColor = '#f28574'
+
+    
+    input.value = row.getElementsByClassName('task')[0].textContent;
+
+    popup.getElementsByClassName('btn')[0].onclick = function (){ UpdateRow(id, input.value) };
+    
+    popup.getElementsByTagName('img')[0].onclick = function () {
+        CancelPopup('edit-popup')
+    }
+
+    input.addEventListener('keyup', function(e){
+        let key = e.which || e.keyCode;
+        if (key == 13){            
+            UpdateRow(id, input.value)
+            row.style.backgroundColor = row_color;
+            
+        } 
+    })
+
+
+   
 }
 
-function DisplayPopup(){
-    document.getElementsByClassName('form-popup')[0].style.display = "flex";
-    console.log("Abriu o popup")
+
+
+function DisplayPopup(str, id){    
+    if(str === 'form-popup'){
+        document.getElementsByClassName('form-popup')[0].style.display = "flex";
+        document.getElementsByClassName('edit-popup')[0].style.display = "none";
+    } else if(str === 'edit-popup'){       
+        if(id !== null){
+           
+            EditPopup(id);           
+          
+          
+        }
+       
+
+    
+
+    } else {
+        
+    }
+    
 }
